c5d31fc132f6d707770c7550935c4e8d236d2586
Jonathan Bedard <jbedard@apple.com>
2022-11-01 11:41:33 -0700

    [run-webkit-tests] Extra thread spawned for each worker https://bugs.webkit.org/show_bug.cgi?id=247324 rdar://101813483
    Reviewed by NOBODY (OOPS!).
    
    * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/__init__.py: Import NullContext before TaskPool.
    * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/multiprocessing_utils.py: Added.
    (Queue.__init__):
    (Queue.close):
    (Queue.empty):
    (Queue.__getstate__): Serialize object.
    (Queue.__setstate__): Unserialize object.
    (Queue.get):
    (Queue.put):
    * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/task_pool.py:
    (_BiDirectionalQueue.__init__): Use multiprocessing_utils.Queue instead of multiprocessing.Queue.
    (_BiDirectionalQueue.send): "closed" is a public variable on multiprocessing_utils.Queue.
    (_BiDirectionalQueue.close): Remove thread join call.
    * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/tests/multiprocessing_utils_unittest.py: Added.
    (QueueUnittest.test_basic):
    (QueueUnittest.test_no_block):
    (QueueUnittest.test_timeout):

---
+++ Tools/Scripts/libraries/webkitcorepy/webkitcorepy/multiprocessing_utils.py
@@ -0,0 +1,125 @@
+# Copyright (C) 2022 Apple Inc. All rights reserved.
+# Copyright (c) 2006-2008, R Oudkerk. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1.  Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2.  Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3.  Neither the name of author nor the names of any contributors may be
+#    used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import time
+import sys
+
+from webkitcorepy import NullContext
+
+if sys.version_info < (3, 0):
+    from multiprocessing import Lock, Pipe
+
+    import Queue as BaseQueue
+
+else:
+    from multiprocessing import context
+    from multiprocessing.queues import connection, _ForkingPickler
+
+    import queue as BaseQueue
+
+    Lock = context._default_context.Lock
+    Pipe = connection.Pipe
+
+
+# A combination of the multiprocessing module's SimpleQueue from Python 2.7.18 and 3.9.6, but with
+# a get() method that support the block and timeout arguments.
+class Queue(object):
+    Empty = BaseQueue.Empty
+
+    def __init__(self):
+        self.closed = False
+        self._reader, self._writer = Pipe(duplex=False)
+        self._rlock = Lock()
+
+        self._poll = self._reader.poll
+        if sys.platform == 'win32':
+            self._wlock = None
+        else:
+            self._wlock = Lock()
+
+    def close(self):
+        self.closed = True
+        self._reader.close()
+        self._writer.close()
+
+    def empty(self):
+        return not self._poll()
+
+    def __getstate__(self):
+        context.assert_spawning(self)
+        return (self._reader, self._writer, self._rlock, self._wlock, self.closed)
+
+    def __setstate__(self, state):
+        (self._reader, self._writer, self._rlock, self._wlock, self.closed) = state
+        self._poll = self._reader.poll
+
+    def get(self, block=True, timeout=None):
+        if self.closed:
+            raise ValueError("Queue is closed")
+        if block and timeout is None:
+            with self._rlock:
+                if sys.version_info < (3, 0):
+                    res = self._reader.recv()
+                else:
+                    res = self._reader.recv_bytes()
+        else:
+            if block:
+                if sys.version_info < (3, 0):
+                    deadline = time.time() + timeout
+                else:
+                    deadline = time.monotonic() + timeout
+            if not self._rlock.acquire(block, timeout):
+                raise self.Empty
+            try:
+                if block:
+                    if sys.version_info < (3, 0):
+                        timeout = deadline - time.time()
+                    else:
+                        timeout = deadline - time.monotonic()
+                    if not self._poll(timeout):
+                        raise self.Empty
+                elif not self._poll():
+                    raise self.Empty
+                if sys.version_info < (3, 0):
+                    res = self._reader.recv()
+                else:
+                    res = self._reader.recv_bytes()
+            finally:
+                self._rlock.release()
+
+        if sys.version_info < (3, 0):
+            return res
+        else:
+            return _ForkingPickler.loads(res)
+
+    def put(self, obj):
+        if sys.version_info >= (3, 0):
+            obj = _ForkingPickler.dumps(obj)
+        with self._wlock or NullContext():
+            if sys.version_info < (3, 0):
+                self._writer.send(obj)
+            else:
+                self._writer.send_bytes(obj)
